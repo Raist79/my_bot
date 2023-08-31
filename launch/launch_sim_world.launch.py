03f8253067dbf0307a544f98bb4d6264bd65e18a
raist <raist@gmx.net>
2023-08-31 16:28:55 +0200

    new file:   .vscode/settings.json 	new file:   config/my_controllers.yaml 	new file:   description/camera.xacro 	modified:   description/lidar.xacro 	modified:   description/robot.urdf.xacro 	new file:   description/ros2_control.xacro 	new file:   launch/launch_sim_world.launch.py 	new file:   worlds/my_world.world

---
+++ launch/launch_sim_world.launch.py
@@ -0,0 +1,108 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+ 
+def generate_launch_description():
+ 
+  # Set the path to the Gazebo ROS package
+  pkg_gazebo_ros = FindPackageShare(package='gazebo_ros').find('gazebo_ros')   
+   
+  # Set the path to this package.
+  pkg_share = FindPackageShare(package='my_bot').find('my_bot')
+ 
+  # Set the path to the world file
+  world_file_name = 'my_world.world'
+  world_path = os.path.join(pkg_share, 'worlds', world_file_name)
+   
+  # Set the path to the SDF model files.
+  gazebo_models_path = os.path.join(pkg_share, 'models')
+  os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
+
+  package_name='my_bot' 
+  
+  rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+  ########### YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE ##############  
+  # Launch configuration variables specific to simulation
+  headless = LaunchConfiguration('headless')
+  use_sim_time = LaunchConfiguration('use_sim_time')
+  use_simulator = LaunchConfiguration('use_simulator')
+  world = LaunchConfiguration('world')
+ 
+  declare_simulator_cmd = DeclareLaunchArgument(
+    name='headless',
+    default_value='False',
+    description='Whether to execute gzclient')
+     
+  declare_use_sim_time_cmd = DeclareLaunchArgument(
+    name='use_sim_time',
+    default_value='true',
+    description='Use simulation (Gazebo) clock if true')
+ 
+  declare_use_simulator_cmd = DeclareLaunchArgument(
+    name='use_simulator',
+    default_value='True',
+    description='Whether to start the simulator')
+ 
+  declare_world_cmd = DeclareLaunchArgument(
+    name='world',
+    default_value=world_path,
+    description='Full path to the world model file to load')
+    
+  # Specify the actions
+   
+  # Start Gazebo server
+  start_gazebo_server_cmd = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzserver.launch.py')),
+    condition=IfCondition(use_simulator),
+    launch_arguments={'world': world}.items())
+ 
+  # Start Gazebo client    
+  start_gazebo_client_cmd = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(os.path.join(pkg_gazebo_ros, 'launch', 'gzclient.launch.py')),
+    condition=IfCondition(PythonExpression([use_simulator, ' and not ', headless])))
+ 
+ 
+  spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'my_bot'],
+                        output='screen')
+  
+  diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+  )
+
+  joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+  )
+  # Create the launch description and populate
+  ld = LaunchDescription()
+ 
+  # Declare the launch options
+  ld.add_action(declare_simulator_cmd)
+  ld.add_action(declare_use_sim_time_cmd)
+  ld.add_action(declare_use_simulator_cmd)
+  ld.add_action(declare_world_cmd)
+  ld.add_action(diff_drive_spawner)
+  ld.add_action(joint_broad_spawner)
+ 
+  # Add any actions
+  ld.add_action(rsp)
+  ld.add_action(start_gazebo_server_cmd)
+  ld.add_action(start_gazebo_client_cmd)
+  ld.add_action(spawn_entity)
+ 
+  return ld
